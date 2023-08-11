1fec7a61f4b5fea56dd61bf16c67df9eb2d76a87
nickdndndn <nickd_96@hotmail.com>
2023-08-11 12:35:11 +0300

    report

--- inference.py
+++ inference.py
@@ -17,29 +17,48 @@ import numpy as np
 
 
 def main():
+    choose_dataset = 'WV3' #or 'WV3'
+
+    if choose_dataset == 'GaoFen2':
+        dataset = eval('GaoFen2')
+        tr_dir = '/home/ubuntu/project/Data/GaoFen-2/train/train_gf2-001.h5'
+        eval_dir = '/home/ubuntu/project/Data/GaoFen-2/val/valid_gf2.h5'
+        test_dir =  '/home/ubuntu/project/Data/GaoFen-2/drive-download-20230623T170619Z-001/test_gf2_multiExm1.h5'
+        checkpoint_dir = 'checkpoints/pnn_model_GF2/pnn_model_GF2_2023_07_17-11_30_23.pth.tar'
+        ms_channel = 4
+    elif choose_dataset == 'WV3':
+        dataset = eval('WV3')
+        tr_dir = '/home/ubuntu/project/Data/WorldView3/train/train_wv3-001.h5'
+        eval_dir = '/home/ubuntu/project/Data/WorldView3/val/valid_wv3.h5'
+        test_dir =  '/home/ubuntu/project/Data/WorldView3/drive-download-20230627T115841Z-001/test_wv3_multiExm1.h5'
+        checkpoint_dir = 'checkpoints/pnn_model_WV3/pnn_model_WV3_2023_07_24-19_19_31.pth.tar'
+        ms_channel = 8
+    else:
+        print(choose_dataset, ' does not exist')
+
     # Prepare device
-    # TODO add more code for server
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
     # Initialize DataLoader
-    train_dataset = WV3(
-        Path("/home/ubuntu/project/Data/WorldView3/train/train_wv3-001.h5"), transforms=[(RandomHorizontalFlip(1), 0.3), (RandomVerticalFlip(1), 0.3)])  # /home/ubuntu/project
+    # Initialize DataLoader
+    train_dataset = dataset(
+        Path(tr_dir), transforms=[(RandomHorizontalFlip(1), 0.3), (RandomVerticalFlip(1), 0.3)])  # /home/ubuntu/project
     train_loader = DataLoader(
         dataset=train_dataset, batch_size=128, shuffle=True, drop_last=True)
 
-    validation_dataset = WV3(
-        Path("/home/ubuntu/project/Data/WorldView3/val/valid_wv3.h5"))
+    validation_dataset = dataset(
+        Path(eval_dir))
     validation_loader = DataLoader(
         dataset=validation_dataset, batch_size=64, shuffle=True)
 
-    test_dataset = WV3(
-        Path("/home/ubuntu/project/Data/WorldView3/drive-download-20230627T115841Z-001/test_wv3_multiExm1.h5"))
+    test_dataset = dataset(
+        Path(test_dir))
     test_loader = DataLoader(
         dataset=test_dataset, batch_size=1, shuffle=False)
 
     # Initialize Model, optimizer, criterion and metrics
-    model = MSDCNN_model(scale=4,  ms_channels=8, mslr_mean=train_dataset.mslr_mean.to(device), mslr_std=train_dataset.mslr_std.to(device), pan_mean=train_dataset.pan_mean.to(device),
+    model = MSDCNN_model(scale=4,  ms_channels=ms_channel, mslr_mean=train_dataset.mslr_mean.to(device), mslr_std=train_dataset.mslr_std.to(device), pan_mean=train_dataset.pan_mean.to(device),
                          pan_std=train_dataset.pan_std.to(device)).to(device)
 
     optimizer = SGD(model.parameters(), lr=0.000001, momentum=0.9)
@@ -83,61 +102,60 @@ def main():
     # load checkpoint
     if continue_from_checkpoint:
         tr_metrics, val_metrics, test_metrics = load_checkpoint(torch.load(
-            'checkpoints/MSDCNN_model_WV3/MSDCNN_model_WV3_2023_07_26-13_25_41.pth.tar'), model, optimizer, tr_metrics, val_metrics, test_metrics)
+            checkpoint_dir), model, optimizer, tr_metrics, val_metrics, test_metrics)
         print('Model Loaded ...')
 
     def scaleMinMax(x):
         return ((x - np.nanmin(x)) / (np.nanmax(x) - np.nanmin(x)))
 
-    idx = 15
     # evaluation mode
     model.eval()
     with torch.no_grad():
         test_iterator = iter(test_loader)
         for i, (pan, mslr, mshr) in enumerate(test_iterator):
-            if idx == i:
-                # forward
-                pan, mslr, mshr = pan.to(device), mslr.to(
-                    device), mshr.to(device)
-                mssr = model(pan, mslr)
-                test_loss = criterion(mssr, mshr)
-                test_metric = test_metric_collection.forward(mssr, mshr)
-                test_report_loss += test_loss
+            # forward
+            pan, mslr, mshr = pan.to(device), mslr.to(
+                device), mshr.to(device)
+            mssr = model(pan, mslr)
+            test_loss = criterion(mssr, mshr)
+            test_metric = test_metric_collection.forward(mssr, mshr)
+            test_report_loss += test_loss
 
-                # compute metrics
-                test_metric = test_metric_collection.compute()
+            # compute metrics
+            test_metric = test_metric_collection.compute()
+            test_metric_collection.reset()
 
-                figure, axis = plt.subplots(nrows=1, ncols=4, figsize=(15, 5))
-                axis[0].imshow((scaleMinMax(mslr.permute(0, 3, 2, 1).detach().cpu()[
-                               0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
-                axis[0].set_title('(a) LR')
-                axis[0].axis("off")
+            figure, axis = plt.subplots(nrows=1, ncols=4, figsize=(15, 5))
+            axis[0].imshow((scaleMinMax(mslr.permute(0, 3, 2, 1).detach().cpu()[
+                            0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
+            axis[0].set_title('(a) LR')
+            axis[0].axis("off")
 
-                axis[1].imshow(pan.permute(0, 3, 2, 1).detach().cpu()[
-                               0, ...], cmap='gray')
-                axis[1].set_title('(b) PAN')
-                axis[1].axis("off")
+            axis[1].imshow(pan.permute(0, 3, 2, 1).detach().cpu()[
+                            0, ...], cmap='gray')
+            axis[1].set_title('(b) PAN')
+            axis[1].axis("off")
 
-                axis[2].imshow((scaleMinMax(mssr.permute(0, 3, 2, 1).detach().cpu()[
-                               0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
-                axis[2].set_title(
-                    f'(c) MSDCNN {test_metric["psnr"]:.2f}dB/{test_metric["ssim"]:.4f}')
-                axis[2].axis("off")
+            axis[2].imshow((scaleMinMax(mssr.permute(0, 3, 2, 1).detach().cpu()[
+                            0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
+            axis[2].set_title(
+                f'(c) MSDCNN {test_metric["psnr"]:.2f}dB/{test_metric["ssim"]:.4f}')
+            axis[2].axis("off")
 
-                axis[3].imshow((scaleMinMax(mshr.permute(0, 3, 2, 1).detach().cpu()[
-                               0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
-                axis[3].set_title('(d) GT')
-                axis[3].axis("off")
+            axis[3].imshow((scaleMinMax(mshr.permute(0, 3, 2, 1).detach().cpu()[
+                            0, ...].numpy())).astype(np.float32)[..., :3], cmap='viridis')
+            axis[3].set_title('(d) GT')
+            axis[3].axis("off")
 
-                plt.savefig('results/Images_WV3.png')
+            plt.savefig(f'results/Images_{choose_dataset}_{i}.png')
 
-                mslr = mslr.permute(0, 3, 2, 1).detach().cpu().numpy()
-                pan = pan.permute(0, 3, 2, 1).detach().cpu().numpy()
-                mssr = mssr.permute(0, 3, 2, 1).detach().cpu().numpy()
-                gt = mshr.permute(0, 3, 2, 1).detach().cpu().numpy()
+            mslr = mslr.permute(0, 3, 2, 1).detach().cpu().numpy()
+            pan = pan.permute(0, 3, 2, 1).detach().cpu().numpy()
+            mssr = mssr.permute(0, 3, 2, 1).detach().cpu().numpy()
+            gt = mshr.permute(0, 3, 2, 1).detach().cpu().numpy()
 
-                np.savez('results/img_array_WV3.npz', mslr=mslr,
-                         pan=pan, mssr=mssr, gt=gt)
+            np.savez(f'results/img_array_{choose_dataset}_{i}.npz', mslr=mslr,
+                        pan=pan, mssr=mssr, gt=gt)
 
 
 if __name__ == '__main__':
