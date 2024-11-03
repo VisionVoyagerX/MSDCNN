c46f6e064acb91e2fc412bfebb472b0550fed1dc
ntantis <ntantis@intralot.com>
2024-11-03 18:16:25 +0200

    Added MSE and RASE metrics

--- inference.py
+++ inference.py
@@ -7,6 +7,8 @@ from torch.nn import MSELoss
 from torch.utils.data import DataLoader
 from torchvision.transforms import Resize, RandomHorizontalFlip, RandomVerticalFlip, RandomRotation
 from torchmetrics import MetricCollection, PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
+from torchmetrics.image import SpectralAngleMapper, ErrorRelativeGlobalDimensionlessSynthesis, RelativeAverageSpectralError, SpectralDistortionIndex
+from torchmetrics.regression import MeanSquaredError
 from torchinfo import summary
 
 from data_loader.DataLoader import DIV2K, GaoFen2, Sev2Mod, WV3, GaoFen2panformer
@@ -17,20 +19,20 @@ import numpy as np
 
 
 def main():
-    choose_dataset = 'GaoFen2' #or 'WV3'
+    choose_dataset = 'WV3' #or 'WV3'
 
     if choose_dataset == 'GaoFen2':
         dataset = eval('GaoFen2')
-        tr_dir = '/home/ubuntu/project/Data/GaoFen-2/train/train_gf2-001.h5'
-        eval_dir = '/home/ubuntu/project/Data/GaoFen-2/val/valid_gf2.h5'
-        test_dir =  '/home/ubuntu/project/Data/GaoFen-2/drive-download-20230623T170619Z-001/test_gf2_multiExm1.h5'
+        tr_dir = 'data/pansharpenning_dataset/GF2/train/train_gf2.h5'
+        eval_dir = 'data/pansharpenning_dataset/GF2/val/valid_gf2.h5'
+        test_dir =  'data/pansharpenning_dataset/GF2/test/test_gf2_multiExm1.h5'
         checkpoint_dir = 'checkpoints/MSDCNN_model_GF2/MSDCNN_model_GF2_2023_07_26-08_30_23.pth.tar'
         ms_channel = 4
     elif choose_dataset == 'WV3':
         dataset = eval('WV3')
-        tr_dir = '/home/ubuntu/project/Data/WorldView3/train/train_wv3-001.h5'
-        eval_dir = '/home/ubuntu/project/Data/WorldView3/val/valid_wv3.h5'
-        test_dir =  '/home/ubuntu/project/Data/WorldView3/drive-download-20230627T115841Z-001/test_wv3_multiExm1.h5'
+        tr_dir = 'data/pansharpenning_dataset/WV3/train/train_wv3.h5'
+        eval_dir = 'data/pansharpenning_dataset/WV3/val/valid_wv3.h5'
+        test_dir =  'data/pansharpenning_dataset/WV3/test/test_wv3_multiExm1.h5'
         checkpoint_dir = 'checkpoints/MSDCNN_model_WV3/MSDCNN_model_WV3_2023_07_26-13_25_41.pth.tar'
         ms_channel = 8
     else:
@@ -67,18 +69,34 @@ def main():
 
     metric_collection = MetricCollection({
         'psnr': PeakSignalNoiseRatio().to(device),
-        'ssim': StructuralSimilarityIndexMeasure().to(device)
+        'ssim': StructuralSimilarityIndexMeasure().to(device),
+        'sam': SpectralAngleMapper().to(device),
+        'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
+        'rase' : RelativeAverageSpectralError().to(device),
+        'mse' : MeanSquaredError().to(device),
     })
 
     val_metric_collection = MetricCollection({
         'psnr': PeakSignalNoiseRatio().to(device),
-        'ssim': StructuralSimilarityIndexMeasure().to(device)
+        'ssim': StructuralSimilarityIndexMeasure().to(device),
+        'sam': SpectralAngleMapper().to(device),
+        'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
+        'rase' : RelativeAverageSpectralError().to(device),
+        'mse' : MeanSquaredError().to(device),
     })
 
     test_metric_collection = MetricCollection({
         'psnr': PeakSignalNoiseRatio().to(device),
-        'ssim': StructuralSimilarityIndexMeasure().to(device)
+        'ssim': StructuralSimilarityIndexMeasure().to(device),
+        'sam': SpectralAngleMapper().to(device),
+        'ergas': ErrorRelativeGlobalDimensionlessSynthesis().to(device),
+        'rase' : RelativeAverageSpectralError().to(device),
+        'mse' : MeanSquaredError().to(device),
     })
+
+    sdi_metric = SpectralDistortionIndex().to(device)
+    sdi_results = []
+
 
     tr_report_loss = 0
     val_report_loss = 0
@@ -121,9 +139,15 @@ def main():
             test_metric = test_metric_collection.forward(mssr, mshr)
             test_report_loss += test_loss
 
-            # compute metrics
-            test_metric = test_metric_collection.compute()
-            test_metric_collection.reset()
+            # Normalize preds and target for SDI
+            # print(mssr.max())
+            preds_normalized = mssr / mssr.max()
+            target_normalized = mshr / mshr.max()
+
+            # Calculate SDI on normalized predictions and targets
+            sdi_value = sdi_metric(preds_normalized, target_normalized)
+            # print(sdi_value)
+            sdi_results.append(sdi_value.item())
 
             figure, axis = plt.subplots(nrows=1, ncols=4, figsize=(15, 5))
             axis[0].imshow((scaleMinMax(mslr.permute(0, 3, 2, 1).detach().cpu()[
@@ -156,6 +180,23 @@ def main():
 
             np.savez(f'results/img_array_{choose_dataset}_{i}.npz', mslr=mslr,
                         pan=pan, mssr=mssr, gt=gt)
+            
+        # compute metrics
+        test_metric = test_metric_collection.compute()
+        test_metric_collection.reset()
+
+        # Compute the average SDI
+        average_sdi = sum(sdi_results) / len(sdi_results)
+
+        # Print final scores
+        print(f"Final scores:\n"
+              f"ERGAS: {test_metric['ergas'].item()}\n"
+              f"SAM: {test_metric['sam'].item()}\n"
+              f"PSNR: {test_metric['psnr'].item()}\n"
+              f"SSIM: {test_metric['ssim'].item()}\n"
+              f"RASE: {test_metric['rase'].item()}\n"
+              f"MSE: {test_metric['mse'].item()}\n"
+              f"D_lambda: {average_sdi:.4f}")
 
 
 if __name__ == '__main__':
